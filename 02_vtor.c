5a9eb81008a696e339617b70b622164f5cfb5b2d
afedotowaa <47708434+afedotowaa@users.noreply.github.com>
2019-04-08 14:33:42 +0700

    Update 02_vtor.c

--- 02_vtor.c
+++ 02_vtor.c
@@ -2,6 +2,7 @@
 #include <stdlib.h> 
 #include <stdbool.h>
 #include <string.h>
+
 typedef struct strr
 {
     int data;
@@ -9,12 +10,14 @@ typedef struct strr
   struct strr * right;
   struct strr * rodit;
 }
+
 strr;
 typedef struct derev
 {
     struct strr * kor;   
   int count;
 }
+
 derev;
 void init(derev* t)
 {
@@ -23,6 +26,7 @@ new_derev->kor = NULL;
     new_derev->count = 0;
     t = new_derev;
 }
+
 int find(derev* t, int zna4, strr* n)
 {
   struct strr * strrstrr;
@@ -47,11 +51,13 @@ strrstrr = t->kor;
   }
   return 1;
 }
+
 int insert(derev* t, int zna4)
 {
   struct strr * n, ** strrstrr, * last_strr = NULL;
 struct strr * en;
 en = malloc(sizeof * en);
+    
 int error = find(t, zna4, en);
   if (error == 0){
     return 1;
@@ -83,14 +89,17 @@ int error = find(t, zna4, en);
   }
   return 0;
 }
+
 int depth(struct strr * n, int deep){
   if (n == NULL){
     return deep;
   }
+    
   int depth1 = depth(n->left, deep + 1);
 int depth2 = depth(n->right, deep + 1);
   return (depth1 > depth2) ? depth1 : depth2;
 }
+
 void printstrr(struct strr * n, int current, int deep, int first){
   if (current == deep){
       if (first > 0){
@@ -110,6 +119,7 @@ printstrr(n->right, current + 1, deep, first + 1);
 printstrr(n, current + 1, deep, first + 1);
   }
 }
+
 void print(struct strr * n)
 {
   int m = depth(n, 0);
@@ -118,11 +128,13 @@ void print(struct strr * n)
 printf("\n");
   }
 }   
+
 void printderev(struct derev * t)
 {
     print(t->kor);
 }
-void print_obhod1(struct strr * n)
+
+void print_obh1(struct strr * n)
 {
   int m = depth(n, 0);
 int flag_derev = 0;
@@ -135,7 +147,8 @@ int flag_derev = 0;
     printstrr(n, 1, i, 0);
   }
 }   
-void print_obhod2(struct derev * t)
+
+void print_obh2(struct derev * t)
 {
     strr* a[15];
 int ab = 0;
@@ -155,6 +168,7 @@ strr* n = t->kor;
         n = a[ab];
         ab -= 1;   
     }
+    
     int flag_derev = 0;
     for (int i = 1; i <= wb; i++){
         if (flag_derev > 0){
@@ -166,28 +180,27 @@ strr* n = t->kor;
     }   
   printf("\n");
 }   
-void print_obhod3(struct strr * n, int flag_derev)
+
+void print_obh3(struct strr * n, int flag_derev)
 {
     if (n->left != NULL){
-        print_obhod3(n->left, flag_derev + 1);
+        print_obh3(n->left, flag_derev + 1);
     } 
     if (n->right != NULL){
-        print_obhod3(n->right, flag_derev + 1);
+        print_obh3(n->right, flag_derev + 1);
     }
     printf("%d", n->data);
     if (flag_derev > 0){
         printf(" ");
     }
 }   
-int main()
-{
-  struct derev * t = malloc(sizeof t);
-init(t);
-  for (int i = 0; i< 7; i++){
+int main(){
+  struct tree * t = malloc(sizeof t);
+  init(t);
+  for (int i = 0; i < 7; i++){
     int a;
-scanf("%d", &a);
-insert(t, a);
+    scanf("%d", &a);
+    insert(t, a);
   }
-  print_obhod2(t);
   return 0;
 }
