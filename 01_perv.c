35cd36b2405216f54a5bfe2ae00595d3d69f9e86
afedotowaa <47708434+afedotowaa@users.noreply.github.com>
2019-04-08 14:27:59 +0700

    Update 01_perv.c

--- 01_perv.c
+++ 01_perv.c
@@ -9,16 +9,15 @@ typedef struct node {
 struct node * right;
 struct node * rodit;
 }
+
 node;
-typedef struct tree
-{
+typedef struct tree{
     struct node * kor;
     
 int count;
 }
 tree;
-void init(tree* t)
-{
+void init(tree* t){
 
     struct tree * new_tree = malloc(sizeof new_tree);
 new_tree->kor = NULL;
@@ -26,8 +25,7 @@ new_tree->count = 0;
 t = new_tree;
 }
 
-int find(tree* t, int zna4, node* n)
-{
+int find(tree* t, int zna4, node* n){
 struct node * str2;
 str2 = t->kor;
 if (t->kor == NULL){
@@ -42,21 +40,22 @@ if (str2 == NULL) {
     n->left = str2->left;
     n->right = str2->right;
     n->rodit = str2->rodit;
-return 0;
-} else if (zna4 > str2->data){
-str2 = str2->right;
-} else {
+    return 0;
+} 
+  else if (zna4 > str2->data){
+    str2 = str2->right;} 
+    else {
 str2 = str2->left;
 }
 }
 return 1;
 }
 
-int insert(tree* t, int zna4)
-{
+int insert(tree* t, int zna4){
 struct node * n, ** strrrr, * pr_str = NULL;
 struct node * e_n;
 e_n = malloc(sizeof * e_n);
+    
 int error_tree = find(t, zna4, e_n);
 if (error_tree == 0){
     return 1;
@@ -64,29 +63,30 @@ if (error_tree == 0){
 strrrr = &t->kor;
 n = t->kor;
 while (1){
-if (n == NULL) {
-    n = * strrrr = malloc(sizeof * n);
-if (n != NULL){
-    n->data = zna4;
-    n->left = NULL;
-    n->right = NULL;
-    n->rodit = pr_str;
-    t->count++;
-return 0;
-} else {
-return 2;
+    if (n == NULL) {
+        n = * strrrr = malloc(sizeof * n);
+    if (n != NULL){
+        n->data = zna4;
+        n->left = NULL;
+        n->right = NULL;
+        n->rodit = pr_str;
+        t->count++;
+        return 0;
+    } else {
+    return 2;
 }
 }
+    
 pr_str = n;
 if (zna4 > n->data){
     strrrr = &n->right;
     n = n->right;
 } else {
-strrrr = &n->left;
-n = n->left;
-}
-}
-return 0;
+    strrrr = &n->left;
+    n = n->left;
+    }
+    }
+    return 0;
 }
 
 int depth(struct node * n, int deep){
@@ -97,6 +97,7 @@ int depth1 = depth(n->left, deep + 1);
 int depth2 = depth(n->right, deep + 1);
 return (depth1 > depth2) ? depth1 : depth2;
 }
+
 void printNode(struct node * n, int current, int deep, int first){
 if (current == deep){
     if (first > 0){
@@ -115,14 +116,15 @@ printNode(n, current + 1, deep, first);
 printNode(n, current + 1, deep, first + 1);
 }
 }
+
 void print(struct node * n)
 {
     
 int m = depth(n, 0);
 for (int i = 1; i <= m; i++){
-printNode(n, 1, i, 0);
-printf("\n");
-}
+    printNode(n, 1, i, 0);
+    printf("\n");
+    }
 }
 
 void printTree(struct tree * t)
@@ -138,7 +140,7 @@ for (int i = 1; i <= m; i++){
 if (flag_tree > 0){
     printf(" ");
 } else {
-flag_tree++;
+    flag_tree++;
 }
 printNode(n, 1, i, 0);
 }
