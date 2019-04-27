c89f68aa11e1a53bc5cd52b061818ead247d8e7e
afedotowaa <47708434+afedotowaa@users.noreply.github.com>
2019-04-27 23:12:23 +0700

    Update 04_1lab.c

--- 04_1lab.c
+++ 04_1lab.c
@@ -2,71 +2,71 @@
 #include <stdlib.h>
 #include <time.h>
 
-int sorting_function(int *l, int *m) {
-	int tmp = *l;
-	*l = *m;
-	*m = tmp;
+int sorting_function(int *rrr, int *ooo) {//пирамидальная
+	int tmp = *rrr;
+	*rrr = *ooo;
+	*ooo = tmp;
 	return 0;
 }
 int main() {
-	time_t start, stop;
-	double itog, sr_itog = 0;
+	clock_t start, stop;
+	double rez, sr_itog = 0;
 	double comp_avg = 0, perm_avg = 0;
 	int comparison, permutation;
-	start = time();
+	start = clock();
 	for (int ch = 0; ch < 1; ch++) {
 		comparison = 0, permutation = 0;
-		int n = 200, top = 0, m;
-		int l[n];
-		for (int i = 0; i < n; i++) { l[i] = rand() % (2 * n) + 1; } 
+		int n = 200, step = 0, ooo;
+		int rrr[n];
+		for (int i = 0; i < n; i++) { rrr[i] = rand() % (2 * n) + 1; } // наилучший {a[i]=n + i - 199;} // наихудший {a[i]=n-i;}
 		while (1) {
-			m = 0;
+			ooo = 0;
 			for (int i = 0; i < n; ++i) {
-				if (i * 2 + 2 + top < n) {
-					if (l[i + top] > l[i * 2 + 1 + top] || l[i + top] > l[i * 2 + 2 + top]) {
+				if (i * 2 + 2 + step < n) {
+					if (rrr[i + step] > rrr[i * 2 + 1 + step] || rrr[i + step] > rrr[i * 2 + 2 + step]) {
 						comparison++;
-						if (l[i * 2 + 1 + top] < l[i * 2 + 2 + top]) {
-							sorting_function(&a[i + top], &l[i * 2 + 1 + top]);
-							m = 1;
+						if (rrr[i * 2 + 1 + step] < rrr[i * 2 + 2 + step]) {
+							sorting_function(&rrr[i + step], &rrr[i * 2 + 1 + step]);
+							ooo = 1;
 							permutation++;
 						}
 						else {
 							comparison++;
-							if (l[i * 2 + 2 + top] < l[i * 2 + 1 + top]) {
-								sorting_function(&l[i + top], &l[i * 2 + 2 + top]);
-								m = 1;
+							if (rrr[i * 2 + 2 + step] < rrr[i * 2 + 1 + step]) {
+								sorting_function(&rrr[i + step], &rrr[i * 2 + 2 + step]);
+								ooo = 1;
 								permutation++;
 							}
 						}
 					}
 				}
-				else if (i * 2 + 1 + top < n) {
+				else if (i * 2 + 1 + step < n) {
 					comparison++;
-					if (l[i + top] > l[i * 2 + 1 + top]) {
-						sorting_function(&l[i + top], &l[i * 2 + 1 + top]);
-						m = 1;
+					if (rrr[i + step] > rrr[i * 2 + 1 + step]) {
+						sorting_function(&rrr[i + step], &rrr[i * 2 + 1 + step]);
+						ooo = 1;
 						permutation++;
 					}
 				}
 			}
-			if (!m) top++;
-			if (top + 2 >= n) break;
+			if (!ooo) step++;
+			if (step + 2 >= n) break;
 			comp_avg += comparison;
 			perm_avg += permutation;
 		}
-		for (int i = 0; i < n; i++) printf("%d ", l[i]);
+		for (int i = 0; i < n; i++) printf("%d ", rrr[i]);
 		printf("\n");
 	}
-	stop = time();
+	stop = clock();
 
-	long double time_itog = (stop - start) / (long double)timeS_PER_SEC;
-	printf("%Lf \n", time_itog / 1000);
+	long double clock_itog = (stop - start) / (long double)CLOCKS_PER_SEC;
+	printf("%Lf \n", clock_itog / 1000);
 	start, stop = 0;
-	time_itog = 0;
+	clock_itog = 0;
 
 	comp_avg = comp_avg / 100;
 	perm_avg = perm_avg / 100;
-	printf("Srednee Sravn: %.2f\n", comp_avg);
-	printf("Srednee Perest: %.2f\n", perm_avg);
+	printf("Average Sravn: %.2f\n", comp_avg);
+	printf("Average Perest: %.2f\n", perm_avg);
 	return 0;
 }
